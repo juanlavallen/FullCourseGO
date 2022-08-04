7835a591ba5d4d93007cc10dcdcd2002ab35691a
juanlavallen <juanlavallen02@gmail.com>
2022-08-04 13:09:04 -0300

    array int

---
+++ 02-DataStructure/Arrays/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var notes [5]int // Default [0 0 0 0 0]
+	fmt.Println(notes)
+
+	notes[0] = 10
+	notes[1] = 20
+	notes[2] = 30
+	notes[3] = 40
+	notes[4] = 50
+
+	fmt.Println(notes)
+	fmt.Println(notes[3])
+}
