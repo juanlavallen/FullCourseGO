51132b34ef2bc97b375c293c52a52ca945b902e2
juanlavallen <juanlavallen02@gmail.com>
2022-08-03 22:30:55 -0300

    variables

---
+++ 01-Introduction/Variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Juan"
+	var age int = 19
+
+	street := 310
+	email := "email@email.com"
+
+	fmt.Println(name, age)
+	fmt.Println(street, email)
+}
