92f5d93348572fa6ed7c805a183d7f0e31d4a516
juanlavallen <juanlavallen02@gmail.com>
2022-08-03 23:31:31 -0300

    function that returns a value

--- 01-Introduction/Functions/main.go
+++ 01-Introduction/Functions/main.go
@@ -6,6 +6,11 @@ func hello(name string) {
 	fmt.Println("Hello", name)
 }
 
+func multiplication(a, b int) int {
+	return a * b
+}
+
 func main() {
 	hello("Mick")
+	fmt.Println(multiplication(2, 9))
 }
