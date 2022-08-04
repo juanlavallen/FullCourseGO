16c6cabc1c4397150a2f035fe0b6b9bd8f86fef1
juanlavallen <juanlavallen02@gmail.com>
2022-08-03 22:52:31 -0300

    arithmetic operators

---
+++ 01-Introduction/ArithmeticOperators/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 205
+	b := 180
+
+	result := a + b
+	fmt.Println("Result:", result)
+
+	result = a - b
+	fmt.Println("Result:", result)
+
+	result = a * b
+	fmt.Println("Result:", result)
+
+	result = a / b
+	fmt.Println("Result:", result)
+}
