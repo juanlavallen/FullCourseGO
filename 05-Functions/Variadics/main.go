94dd81dabe401a7384aa1c6f0b8a3aed9d80f9bf
juanlavallen <juanlavallen02@gmail.com>
2022-08-26 15:38:10 -0300

    Function return + 1 value

--- 05-Functions/Variadics/main.go
+++ 05-Functions/Variadics/main.go
@@ -10,7 +10,19 @@ func sum(numbers ...int) int {
 	return total
 }
 
+func sum2(name string, numbers ...int) (string, int) {
+	message := fmt.Sprintf("The result of %s sum is", name)
+	var total int
+	for _, number := range numbers {
+		total += number
+	}
+	return message, total
+}
+
 func main() {
 	result := sum(150, 350)
 	fmt.Println(result)
+
+	message, result := sum2("Lionel", 2, 10)
+	fmt.Println(message, result)
 }
