c449384f420e13f3491066d7444232c70493135a
juanlavallen <juanlavallen02@gmail.com>
2022-09-05 19:37:00 -0300

    Inheritance

--- 07-Struct-Interfaces/Inheritance/main.go
+++ 07-Struct-Interfaces/Inheritance/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+type Person struct {
+	firstname string
+	lastname  string
+	age       int
+}
+
+type Employee struct {
+	Person
+	salary     float64
+	profession string
+}
+
+func (e *Employee) format() {
+	fmt.Printf("Name: %s\nAge: %s\nProfession: %s\n", e.firstname, e.lastname, e.profession)
+}
+
+func main() {
+	employee := Employee{salary: 100, profession: "Programmer"}
+	employee.firstname = "Juan"
+	employee.lastname = "Lavallen"
+	employee.age = 20
+	fmt.Println(employee)
+
+	employee.format()
 }
