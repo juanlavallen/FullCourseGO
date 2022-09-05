da4b1cc74059b25558168ebc5efb36939d943d11
juanlavallen <juanlavallen02@gmail.com>
2022-09-05 19:08:22 -0300

    Instance

--- 07-Struct-Interfaces/main.go
+++ 07-Struct-Interfaces/main.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
+
+// Struct
 type Person struct {
 	name string
 	age  int
 }
 
 func main() {
+	// Instance
+	// person := Person{"Paul", 34}
+	person := Person{name: "Paul", age: 34}
+	fmt.Println(person)
 }
